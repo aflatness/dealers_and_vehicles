52137e4d054843681a9e3ba912066b75908e4d6a
aflatness <aflatness@gmail.com>
2021-04-14 16:01:29 -0600

    Tests written for vehicleData and Dealers controller functions

--- test.js
+++ test.js
@@ -1,14 +1,18 @@
 const { expect } = require('chai');
 const controllers = require('./src/controllers');
 
-let datasetId, vehicles, dealers;
+const { getDatasetId, getVehicleData, getDealerData, getVehicles } = controllers;
+let datasetId, vehicles, vehicleData;
+let dealers = [];
 
 describe('Controller functions', function () {
-  const { getDatasetId, getVehicleData, getDealerData, getVehicles } = controllers;
 
-  beforeEach( async function () {
+  this.timeout(6000);
+
+  before( async function () {
     datasetId = await getDatasetId();
-  })
+  });
+
   describe('DatasetId API', function () {
     it('should return promise', function (done) {
       expect(getDatasetId()).to.be.a('promise');
@@ -16,7 +20,6 @@ describe('Controller functions', function () {
     });
 
     it('should resolve into a string', async function () {
-      // const datasetId = await getDatasetId();
       expect(datasetId).to.be.a('string');
     });
   });
@@ -28,15 +31,56 @@ describe('Controller functions', function () {
     });
 
     it('should resolve into an array', async function () {
-      const vehicleData = await getVehicles(datasetId);
-      expect(vehicleData).to.be.an('array');
+      vehicles = await getVehicles(datasetId);
+      expect(vehicles).to.be.an('array');
     });
 
+
     it('should resolve into an array of numbers', async function () {
-      const vehicleData = await getVehicles(datasetId);
-      vehicleData.forEach(vehicleId => {
+      vehicles = await getVehicles(datasetId);
+      vehicles.forEach(vehicleId => {
         expect(vehicleId).to.be.a('number');
       });
+    });
+  });
+
+  describe('vehicleData API', function () {
+    it('should return promise', function (done) {
+      expect(getVehicleData(datasetId, vehicles[0])).to.be.a('promise');
+      done();
+    });
+
+    it('should resolve into an object', async function () {
+      vehicleData = await getVehicleData(datasetId, vehicles[0]);
+
+      expect(vehicleData).to.be.an('object');
+      expect(vehicleData).to.have.property('dealerId');
+      expect(vehicleData).to.have.property('make');
+      expect(vehicleData).to.have.property('model');
+      expect(vehicleData).to.have.property('year');
+    });
+  });
+
+  describe('dealers API', function () {
+    let id;
+
+    before((done) => {
+      id = vehicleData.dealerId;
+      done();
+    })
+
+    it('should return promise', function (done) {
+      expect(getDealerData(datasetId, id)).to.be.a('promise');
+      done();
+    });
+
+    it('should resolve into an object', async function () {
+      const dealerData = await getDealerData(datasetId, id);
+
+      expect(dealerData).to.be.an('object');
+      expect(dealerData).to.have.property('dealerId');
+      expect(dealerData.dealerId).to.equal(id);
+      expect(dealerData).to.have.property('name');
     });
   });
 });
